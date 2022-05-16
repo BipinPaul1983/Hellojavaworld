18294b195f5b26c31844067189ed8aba334854b9
BIPINPAUL <bipinpaul83@gmail.com>
2022-05-16 12:13:55 +0100

    Second commit

--- src/main/java/PainterCalculator.java
+++ src/main/java/PainterCalculator.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class PainterCalculator {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
@@ -10,13 +12,34 @@ public class PainterCalculator {
         System.out.println("Enter the values in meter square only");
         System.out.println("Please enter the details below to find the area of wall to be painted");
 
-       /*
-       int NumberOfRooms = Integer.parseInt(input.nextLine());
-        System.out.println("How many rooms are there?");
-        if(NumberOfRooms>0);
-        */
+        //for (int i = 1; i <= 10; i++) {
+            //System.out.println(i);
 
-        System.out.println("Height of the wall to paint:");
+            System.out.println("How many walls are there?");
+            int NumberOfWalls = Integer.parseInt(input.nextLine());
+            if (NumberOfWalls > 0)
+            //{
+            //System.out.println(NumberOfWalls +  "Walls to paint");
+            {
+                for (int NumberOfWall = NumberOfWalls; NumberOfWall < 10; NumberOfWall++) {
+                    System.out.println("Number Of Walls: " + (NumberOfWall));
+                    System.out.println("Height of the wall to paint:");
+                    double HeightOfWall = Double.parseDouble(input.nextLine());
+                    System.out.println("Width of the wall to paint:");
+                    double WidthOfWall = Double.parseDouble(input.nextLine());
+                    double AreaOfWall = (NumberOfWall) * HeightOfWall * WidthOfWall;
+                    System.out.println("The area of wall to paint is:" + AreaOfWall);
+                }
+            }
+            // else{
+            // System.out.println("Cannot proceed");
+            //exit();
+        }
+        // for(int i=0;i<NumberOfRooms;i++)
+
+
+
+       /* System.out.println("Height of the wall to paint:");
         double HeightOfWall = Double.parseDouble(input.nextLine());
         System.out.println("Width of the wall to paint:");
         double WidthOfWall = Double.parseDouble(input.nextLine());
@@ -48,6 +71,9 @@ public class PainterCalculator {
         System.out.println("Amount of paint required to paint this area is :" + TotalAmountOfPaintRequired + "litres");
 
 
+    }*/
+
     }
-}
+
+
 
